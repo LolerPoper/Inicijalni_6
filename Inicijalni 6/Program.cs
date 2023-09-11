93c13af233718f4eb489de59f4ec3aba748ccfc0
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-09-11 11:08:03 +0200

    Add files via upload

---
+++ Inicijalni 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, broj;
+            double sum = 0;
+
+            Console.WriteLine("Unesite broj članova reda: ");
+            broj = Convert.ToInt16(Console.ReadLine());
+
+            for(int i = 0; i <= broj; i++)
+            {
+                sum = sum + 1/Math.Pow(a, i);
+            }
+
+            Console.WriteLine("Suma reda je " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
